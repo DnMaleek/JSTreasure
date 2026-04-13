b1a2664943e821b59307140e8f2fda3014089831
DnMaleek <noeldn25@gmail.com>
2026-04-13 12:01:51 +0300

    bulky server fixed

--- bulk_mini_server.js
+++ bulk_mini_server.js
@@ -7,10 +7,11 @@ app.use(express.json());
 
 (async () => {
     const db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME
+        host: process.env.MYSQLHOST,
+        user: process.env.MYSQLUSER,
+        password: process.env.MYSQLPASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQLPORT
     });
 
     console.log("Bulk import DB connected");
